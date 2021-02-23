32808e3f97c7aba3e53ca583459f8e20b6bb5432
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-02-22 22:44:25 -0700

    capatilized PORT

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const htmlRoutes = require('./routes/html_routes')
 const app = express()
-const port = process.env.port||3000
+const PORT = process.env.PORT||3000
 //compiling what comes through port 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public")) 
 app.use("/",htmlRoutes)
-app.listen(port, () => console.log(`running on ${port}`))
+app.listen(PORT, () => console.log(`running on ${PORT}`))
