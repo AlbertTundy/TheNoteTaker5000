c7208b61840f0d47a4029ce9cca52311578a33a5
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-02-22 22:53:09 -0700

    bug fixing

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const aprRoutes = require("./routes/api_routes")
 const htmlRoutes = require('./routes/html_routes')
 const app = express()
-const port = process.env.port||3000
+const PORT = process.env.PORT||3000
 //compiling what comes through port 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public")) 
 app.use("/api", aprRoutes)
 app.use("/",htmlRoutes)
-app.listen(port, () => console.log(`running on ${port}`))
+app.listen(PORT, () => console.log(`running on ${PORT}`))
