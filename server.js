f84de11fc260bc1ca511acf69553307d09e9f339
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-02-22 22:26:33 -0700

    adding to heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const aprRoutes = require("./routes/api_routes")
 const htmlRoutes = require('./routes/html_routes')
 const app = express()
 const port = process.env.port||3000
@@ -6,5 +7,6 @@ const port = process.env.port||3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public")) 
+app.use("/api", aprRoutes)
 app.use("/",htmlRoutes)
 app.listen(port, () => console.log(`running on ${port}`))
